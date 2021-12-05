6d2efdb8e2445535255c701c7ddd01ec7aa6c560
Saurabhkumar3110 <94735523+Saurabhkumar3110@users.noreply.github.com>
2021-12-05 14:05:50 +0530

    Lab Assignment 2
    Submitting lab assignment 2

---
+++ lab 2/Assignment_2q2_b.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            char c = (char)num;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
